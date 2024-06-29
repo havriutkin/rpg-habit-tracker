5a0d91687e654cc2d1c561ce7f277efb6b13eda6
Vladyslav Havriutkin <havriutkin@gmail.com>
2024-06-28 22:01:27 -0400

    Implement custom error class

---
+++ back-end/src/errors/CustomError.ts
@@ -0,0 +1,12 @@
+class CustomError extends Error {
+    responseCode: number;
+    responseMessage: string;
+    loggerMessage: string;
+
+    constructor(responseCode: number, responseMessage: string, loggerMessage: string) {
+        super();
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+        this.loggerMessage = loggerMessage;
+    }
+}
